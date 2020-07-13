20b1498e9c509e786b6b9906fd356ef74c020840
Alonso Parra <alonsogparra@gmail.com>
2020-07-13 10:58:59 -0400

    rootReducer updated with auth reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer  } from 'redux-firestore';
+import authReducer from './auth';
 
 export default combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  auth: authReducer
 });
