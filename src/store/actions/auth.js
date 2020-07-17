20b5d8d428fc9ced5a952862f26cc91b332d02f5
Alonso Parra <alonsogparra@gmail.com>
2020-07-17 10:19:12 -0400

    action creator built signUp to make the connection with firebase and create the user for GuitaReact

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -16,3 +16,28 @@ export const signIn = (credentials) => {
     })
   }
 }
+
+export const signUp = (newUser) => {
+  return (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+
+    firebase.auth().createUserWithEmailAndPassword(
+      newUser.email,
+      newUser.password
+    )
+    .then(res => {
+      return firestore.collection('users').doc(res.user.uid).set({
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        initials: newUser.firstName[0] + newUser.lastName[0]
+      })
+    })
+    .then(() => {
+      dispatch({ type: actionTypes.SIGNUP_SUCCESS });
+    })
+    .catch(error => {
+      dispatch({ type: actionTypes.SIGNUP_ERROR, error });
+    })
+  }
+}
