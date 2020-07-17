88ef2c141b0ef31144924ffebbe9ed19a4e1d37c
Alonso Parra <alonsogparra@gmail.com>
2020-07-17 10:18:00 -0400

    actionTypes added, SIGNUP_SUCCESS, SIGNUP_ERROR

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,4 +1,7 @@
 export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
 export const SIGNIN_ERROR = 'SIGNIN_ERROR';
 
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+
 export const CHANGE_THEME = 'CHANGE_THEME';
